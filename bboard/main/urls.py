4bb8b2aebd2fea5aa18af31207d98f6172ff5a51
Andrey <aws110615@gmail.com>
2021-11-12 04:09:58 +0500

    Added a model AdvUser. Added user login and logout functions on the site

--- bboard/main/urls.py
+++ bboard/main/urls.py
@@ -5,6 +5,9 @@ from .views import *
 
 app_name = 'main'
 urlpatterns = [
+    path('accounts/profile/', profile, name="profile"),
+    path('accounts/logout/', BBLogoutView.as_view(), name="logout"),
+    path('accounts/login/', BBLoginView.as_view(), name="login"),
     path('<str:page>/', other_page, name='other'),
     path('', index, name='index'),
 ]
--- bboard/main/views.py
+++ bboard/main/views.py
@@ -2,6 +2,24 @@ from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from django.template import TemplateDoesNotExist
 from django.template.loader import get_template
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+
+
+class BBLogoutView(LoginRequiredMixin, LogoutView):
+    template_name = 'main/logout.html'
+
+
+class BBLoginView(LoginView):
+    template_name = 'main/login.html'
+
+
+@login_required
+def profile(request):
+    return render(request, 'main/profile.html')
 
 
 def other_page(request, page):
