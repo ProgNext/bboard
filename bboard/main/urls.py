dd4a9caacbac4c84bd696c61f40fa63c0c236698
Andrey <aws110615@gmail.com>
2021-11-13 01:29:11 +0500

    Added function delete of user.

--- bboard/main/urls.py
+++ bboard/main/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('accounts/register/done/', RegisterDoneView.as_view(), name="register_done"),
     path('accounts/register/', RegisterUserView.as_view(), name="register"),
     path('accounts/password/', BBPasswordChangeView.as_view(), name="password_change"),
+    path('accounts/profile/delete/', DeleteUserView.as_view(), name="profile_delete"),
     path('accounts/profile/change', ChangeUserInfoView.as_view(), name="profile_change"),
     path('accounts/profile/', profile, name="profile"),
     path('accounts/logout/', BBLogoutView.as_view(), name="logout"),
